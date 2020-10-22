761dc01b4fe8be80b2c82e7b2c8d73eb088704cc
Julia Matos <juliaribeiro@outlook.com>
2020-10-22 20:48:52 -0300

    created information section

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ function App() {
   const greeting = "Hello";
   const name = "Hello";
 
-  return (<>
+  return (
+  <div className="card">
     <div className="badge">
       <h1 key ={greeting}>{greeting}</h1>
     </div>
-      
-     </>
+    <h1>I'm {name}</h1>
+
+      <p className="top-border">Information Systems student</p>
+      <p className="top-border">Niterói, Rio de Janeiro, Brazil</p>
+      <p className="top-border">Find me on:</p>
+
+  </div>
   );
 }
 
