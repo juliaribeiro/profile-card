6ae9400c006a158acf6eaa1fb3bc6b9e15755f42
Julia Matos <juliaribeiro@outlook.com>
2020-10-22 20:38:44 -0300

    created badge

--- src/App.js
+++ src/App.js
@@ -3,23 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const greeting = "Hello";
+  const name = "Hello";
+
+  return (<>
+    <div className="badge">
+      <h1 key ={greeting}>{greeting}</h1>
     </div>
+      
+     </>
   );
 }
 
